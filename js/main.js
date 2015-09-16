216bc391486b6e303dfc7950d6df8bdf228b703b
Benson Mburu <nguruben@gmail.com>
2015-09-16 12:24:28 +0300

    js pop up effect

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
+    $(".open").click(function () {
+        $(".popup").fadeIn(500);
+        $(".cover").fadeTo(500, 0.5);
+    });
 
+    $(".close").click(function () {
+        $(".popup").fadeOut(500);
+        $(".cover").fadeOut(500);
+    });
